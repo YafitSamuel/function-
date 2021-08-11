41238ac80a05360e76287eeee6a991d47c874506
YafitSamuel <gyafit642@gmail.com>
2021-08-11 07:57:05 +0300

    commit

--- function.js
+++ function.js
@@ -170,3 +170,120 @@
 //     }
 // }
 
+// --------------------------למידה עצמית ומחודשת --------------------------------------
+// -------------------------1-------------------------
+// function printMyName(){
+//     var username = "yafit"
+//     for(var i=0;i<5;i++){
+//         console.log(username);
+//     }
+// }
+//  printMyName();
+
+
+//  -------------------------2-------------------------
+
+// function printMyName(myName){
+//     for(var i=0;i<5;i++){
+//       console.log(myName)
+// }
+// }
+// printMyName("yafit");
+
+// -------------------------3-------------------------
+
+// function twoNumbers(){
+//   num1=8
+//   nun2=3
+//   console.log(num1*nun2)
+// }
+
+// twoNumbers()
+
+// -------------------------4-------------------------
+
+// function twoNumbers(num1, num2){
+// var sum=num1+num2
+// console.log(sum)
+
+// }
+// twoNumbers(3,5)
+
+// -------------------------5--------------------------
+
+// function twoNumbers(num1, num2) {
+//     if (num1 < num2) {
+//         return num1
+//     }
+//     else if (num1 > num2) {
+//         return num2
+//     }
+// }
+// var x=twoNumbers(3, 2)
+// console.log(x)
+
+// -------------------------6----------------------------
+
+// function fullNumber(num) {
+//     while (num > 0) {
+//         var modulu = num % 10
+//         var num = (num / 10) - (num / 10)
+//         var sum = modulu + modulu
+//         return (sum)
+//     }
+// }
+// var x = fullNumber(564)
+// console.log(x)
+
+// ------------------------8----------------------------
+
+
+// function mahrozet(username) {
+//     return username.length
+// }
+// var x=mahrozet("yafit")
+// console.log(x)
+
+
+// ------------------------9----------------------------
+// var userinput = Number(prompt("enter a number"));
+// function maarah(array) {
+//     for (var i = 0; i <array.length; i++) {
+//         if (array[i] == userinput) {
+//             array.splice(i, 1)
+//             return (i)
+//         }
+//     }
+//     return ("no exist")
+// }
+// var x = maarah([5, 3, 4, 57])
+// console.log(x)
+
+// -----------------------10---------------------------
+// function maarah(array) {
+//     for (var i = 0; i < array.length; i++) {
+//         array[i] = array[i] * 3
+//          return (array)
+//     }
+// }
+// var x = maarah([7, 1, 3])
+// console.log(x)
+
+// -----------------------11---------------------------
+function maarah(array){
+    for (var i = 0; i < array.length; i++) {
+        for(var j =+1; j < array.length; j++){
+            if(array[i]==array[j]){
+                array.splice (j,1)
+            }
+        }
+       
+
+        }
+        return(array)
+    }
+
+var x=maarah([5,5,5,5,9,8,7,,66,66])
+console.log(x)
+
+
